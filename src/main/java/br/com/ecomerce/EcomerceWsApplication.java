87633ca23efee2ef88d698bb264b2fdb5e9fd714
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-09-25 19:34:18 -0300

    Projeto Criado

---
+++ src/main/java/br/com/ecomerce/EcomerceWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ecomerce/EcomerceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
