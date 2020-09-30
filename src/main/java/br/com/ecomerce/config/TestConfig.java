27a48bf32e1ff178cb5276ccad087667a42ba333
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-09-29 22:29:34 -0300

    Order, Instant, ISO 8601

--- src/main/java/br/com/ecomerce/config/TestConfig.java
+++ src/main/java/br/com/ecomerce/config/TestConfig.java
@@ -1,5 +1,6 @@
 package br.com.ecomerce.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.ecomerce.entities.Order;
 import br.com.ecomerce.entities.User;
 import br.com.ecomerce.repositories.UserRepository;
 
@@ -23,7 +25,7 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2)); 
 		
 	}
 
--- src/main/java/br/com/ecomerce/entities/User.java
+++ src/main/java/br/com/ecomerce/entities/User.java
@@ -1,13 +1,18 @@
 package br.com.ecomerce.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +24,9 @@ public class User implements Serializable{
 	private String email; 
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {		
 	}
@@ -70,6 +78,28 @@ public class User implements Serializable{
 
 	public void setSenha(String senha) {
 		this.password = senha;
+	}
+	
+	
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
