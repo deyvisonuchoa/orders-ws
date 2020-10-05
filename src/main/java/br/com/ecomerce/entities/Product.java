7b09335effc587772d5c02e3975b16c38cc29a5c
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-10-05 12:47:24 -0300

    products

--- src/main/java/br/com/ecomerce/entities/Product.java
+++ src/main/java/br/com/ecomerce/entities/Product.java
@@ -1,6 +1,5 @@
 package br.com.ecomerce.entities;
 
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,32 +8,47 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name= "tb_category")
-public class Category implements Serializable{
+public class Product implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
-	
-	public Category() {
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
 		
 	}
-
-	public Category(Long id, String name) {
+	
+	public Product(Long id, String name, String description, double price, String imgUrl,
+			Set<Category> categories) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.categories = categories;
+	}
+
+	public Product(Long id, String name, String description, double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -53,8 +67,32 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -73,7 +111,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -81,11 +119,7 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
+	
+	
 
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + "]";
-	}
-	
-	
 }
--- src/main/java/br/com/ecomerce/resources/ProductResource.java
+++ src/main/java/br/com/ecomerce/resources/ProductResource.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.ecomerce.entities.User;
-import br.com.ecomerce.services.UserService;
+import br.com.ecomerce.entities.Product;
+import br.com.ecomerce.services.ProductService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/products")
+public class ProductResource {
 	
 	@Autowired
-	private UserService service;
+	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){		
-		//User u = new User(1L, "Maria", "maria@gmail.com", "9999994434", "123456");	
-		List<User> lista = service.findAll();
+	public ResponseEntity<List<Product>> findAll(){		
+		//Product u = new Product(1L, "Maria", "maria@gmail.com", "9999994434", "123456");	
+		List<Product> lista = service.findAll();
 		return ResponseEntity.ok().body(lista);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User obj = service.findById(id);
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = service.findById(id);
 		return ResponseEntity.ok(obj);
 	}
 	
