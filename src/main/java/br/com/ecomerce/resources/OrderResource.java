bd012548c2a83b2ca700becdd3267d1a91057940
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-10-08 22:20:11 -0300

    Orders POST , PUT, DELETE

--- src/main/java/br/com/ecomerce/resources/OrderResource.java
+++ src/main/java/br/com/ecomerce/resources/OrderResource.java
@@ -1,14 +1,21 @@
 package br.com.ecomerce.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.ecomerce.entities.Order;
 import br.com.ecomerce.entities.Order;
 import br.com.ecomerce.services.OrderService;
 
@@ -30,6 +37,29 @@ public class OrderResource {
 	public ResponseEntity<Order> findById(@PathVariable Long id){
 		Order obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@PostMapping
+	private ResponseEntity<Order> insert(@RequestBody Order entity){	
+		
+		entity = service.insert(entity);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(entity.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(entity);		
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order obj){
+		obj = service.update(id, obj);	
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
--- src/main/java/br/com/ecomerce/services/OrderService.java
+++ src/main/java/br/com/ecomerce/services/OrderService.java
@@ -3,12 +3,20 @@ package br.com.ecomerce.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.ecomerce.entities.Order;
+import br.com.ecomerce.entities.Order;
 import br.com.ecomerce.entities.User;
 import br.com.ecomerce.repositories.OrderRepository;
+import br.com.ecomerce.services.exceptions.BusinessException;
+import br.com.ecomerce.services.exceptions.DatabaseException;
+import br.com.ecomerce.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -22,6 +30,39 @@ public class OrderService {
 	
 	public Order findById(Long id) {
 		Optional<Order> order = repository.findById(id);
-		return order.get();
+		return order.orElseThrow( () -> new ResourceNotFoundException(id) );
+	}
+	
+	public Order insert(Order entity) {		
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Order update(Long id, Order obj) {
+		try {
+		Order entity = repository.getOne(id); //get reference
+		
+		updateData(entity, obj);
+		return repository.save(entity);
+		
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setMoment(obj.getMoment());
+		entity.setStatus(obj.getStatus());
+		entity.setUser(obj.getUser());
+		entity.setPayment(obj.getPayment());
 	}
 }
